0a0626a9daed76403db16f777a3b2bc7c6948313
mahamudsourov <sourovm554@gmail.com>
2025-03-18 00:35:16 +0600

    problem_02

---
+++ lib/problem2.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Test());
+}
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const TestScreen(),
+    );
+  }
+}
+
+class TestScreen extends StatefulWidget {
+  const TestScreen({super.key});
+
+  @override
+  _TestScreenState createState() => _TestScreenState();
+}
+
+class _TestScreenState extends State<TestScreen> {
+  String displayText = "Press the Button";
+
+  void updateText() {
+    setState(() {
+      displayText = "Button Pressed";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Button App")),
+        backgroundColor: const Color.fromARGB(255, 76, 172, 175),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              displayText,
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 211, 103, 70),
+                fontFamily: 'DMSans',
+              ),
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+            ElevatedButton(
+              onPressed: updateText,
+              child: const Text("Press here"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
